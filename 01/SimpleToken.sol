a6fef2a232199609872402aa5e647792684d05e4
qbzzt <qbzzt1@gmail.com>
2017-12-25 17:38:25 -0600

    Update and rename SimpleCoin.sol to SimpleToken.sol

--- 01/SimpleToken.sol
+++ 01/SimpleToken.sol
@@ -1,26 +1,23 @@
 pragma solidity ^0.4.0;
 
-contract SimpleCoin {
+contract SimpleToken {
     // For display purposes
     string public name;
     string public symbol;
     
-    // The issuer. The address type is used to identify users, contracts, and
-    // so on
+    // The issuer, the entity that can mint and melt tokens.
     address public issuer;
-    
-    // Solidity uses integers. To allow coins to be divided, act as if 
-    // 10^18 is a single coin.
+ 
     uint8 public decimals = 18;
-
-    uint256 public totalSupply;
+    uint256 public totalSupply;    
     
-    mapping (address => uint256) balance;
+    // The balances of each user
+    mapping (address => uint256) public balance;
 
-    // Contract constructor, called once when a contract is created
-    function SimpleCoin(string tokenName, string tokenSymbol) public {
+    function SimpleToken(string tokenName, string tokenSymbol) public {
         // In the beginning, there is no money
         totalSupply = 0; 
+        
         issuer = msg.sender;
         
         // Name and symbol for display purposes
@@ -33,6 +30,9 @@ contract SimpleCoin {
     function mint(uint256 _amount) public {
         // Only the issuer may mint or melt
         require(msg.sender == issuer);
+        
+        // Avoid overflows
+        require(totalSupply + amount > totalSuppy);
         
         balance[issuer] += _amount;
         totalSupply += _amount;
@@ -54,19 +54,17 @@ contract SimpleCoin {
         totalSupply -= _amount;
     }    
     
-    // Get the total number in circulation
-    //
-    // constant functions do not change the contract state, which means 
-    // they are a lot cheaper to run
-    function marketTotal() public constant returns (uint256) {
+    // Get the total number of tokens in circulation
+    function marketTotal() public view returns (uint256) {
         return totalSupply - balance[issuer];
     }
     
     
     // Get my current balance
-    function getBalance() public constant returns (uint256) {
+    function getBalance() public view returns (uint256) {
         return balance[msg.sender];
     }
+    
     
     // Pay an amount
     function pay(uint256 _amount, address _destination) public {
@@ -77,6 +75,5 @@ contract SimpleCoin {
         
         balance[_destination] += _amount;
         balance[msg.sender] -= _amount;
-    }
-    
+    }   
 }
